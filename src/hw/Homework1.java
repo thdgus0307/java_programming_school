cf4886b4ed326adda275877c8e5a57f122f8443c
thdgus0307 <bsh0374@gmail.com>
2025-09-14 14:15:18 +0900

    Homework1

---
+++ src/hw/Homework1.java
@@ -0,0 +1,16 @@
+package hw;
+
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for(int i =0; i<5;i++)
+        {
+            System.out.print("정수를 입력하세요: ");
+            sum += sc.nextInt();
+            System.out.printf("현재까지 입력된 정수의 합은 %d입니다.\n", sum);
+        }
+    }
+}
--- src/lecture/Main.java
+++ src/lecture/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int num = 0;
+        System.out.println("num = "+ num);
     }
 }
